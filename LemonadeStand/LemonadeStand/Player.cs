e0032b853e4b0d2687cc5b156e6976d38100231a
Brian Pennington <srsrec@gmail.com>
2018-10-29 11:12:32 -0500

    created Player class

---
+++ LemonadeStand/LemonadeStand/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Player
+    {
+        public string playerName;
+        public int playerBudget = 50; // dollars, to start
+
+        public Player()
+        {
+                
+        }
+
+
+    }
+}
