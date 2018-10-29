6c0c44286f1c006c071af0130ed958e9fa93e819
Brian Pennington <srsrec@gmail.com>
2018-10-29 14:47:41 -0500

    added Store classes and children classes

---
+++ LemonadeStand/LemonadeStand/OldiStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class OldiStore:Store
+    {
+        public int oldiMoldiBagOfLemons = 2;
+        public int oldiBagOfTeethRottingSugar = 2;
+    }
+}
---
+++ LemonadeStand/LemonadeStand/Store.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    abstract class Store
+    {
+        public int bagOfIce = 2;
+
+    }
+}
